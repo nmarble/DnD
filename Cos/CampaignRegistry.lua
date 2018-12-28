8f8a53bf8351f1bfdef1e858399234ca506f98cc
nmarble <nmarble@uccs.edu>
2018-12-28 00:46:43 -0700

    Init push

---
+++ Cos/CampaignRegistry.lua
@@ -0,0 +1,6 @@
+{
+	["OptHRNH"] = "random",
+	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel,vehicle",
+	["setup"] = true,
+	["OptINIT"] = "on",
+}
---
+++ NickCamp/CampaignRegistry.lua
@@ -0,0 +1,6 @@
+{
+	["OptHRFC"] = "both",
+	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel,spell,skill,race,class,feat,background",
+	["OptNNPC"] = "append",
+	["OptTNAM"] = "on",
+}
